dc5273fe7327aed9404d70c28ff18ba4023ecc5b
Miroljub Enjaković <miroljub.enjakovic@supper.london>
2020-07-03 22:07:28 +0200

    Implement EuclideanAlgorithm

---
+++ src/main/java/exercises/EuclideanAlgorithm.java
@@ -0,0 +1,25 @@
+package exercises;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class EuclideanAlgorithm {
+    public static void main(String[] args) {
+        StdOut.println("Enter first number:");
+        int x = StdIn.readInt();
+        StdOut.println("Enter second number:");
+        int y = StdIn.readInt();
+
+        StdOut.printf("Greatest common divisor is %d\r\n", findGreatestCommonDivisor(x, y));
+    }
+
+    private static int findGreatestCommonDivisor(int firstNumber, int secondNumber) {
+        if (secondNumber == 0) {
+            return firstNumber;
+        }
+
+        int newSecondNumber = firstNumber % secondNumber;
+
+        return findGreatestCommonDivisor(secondNumber, newSecondNumber);
+    }
+}
